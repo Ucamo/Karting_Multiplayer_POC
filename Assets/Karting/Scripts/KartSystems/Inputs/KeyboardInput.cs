6e08395218a46dd36004666e5d581e5869126467
Uriel Carrillo <carrillouriel@gmail.com>
2021-02-24 23:35:56 -0700

    initial commit, 2 player modes, local

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public string forward;
+        public string backward;
+        public string left;
+        public string right;
+
+        public override Vector2 GenerateInput() {
+
+            int valY =0;
+            int valX=0;
+
+            if (Input.GetKey(forward))
+            {
+                valY=1;
+            }
+            if (Input.GetKey(backward))
+            {
+                valY=-1;
+            }
+            if (Input.GetKey(right))
+            {
+                valX=1;
+            }
+            if (Input.GetKey(left))
+            {
+                valX=-1;
+            }
+            return new Vector2 {
+                x = valX,
+                y = valY
+            };
+        }
+    }
+}
